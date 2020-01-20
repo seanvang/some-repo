b71ed7c892cc0cbc9098b0d4a828e7393b5a6f37
Sean Anggani <seanvang@umich.edu>
2020-01-20 15:28:33 +0700

    redep

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello heck World!'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
