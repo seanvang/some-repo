ec6b21708726a5feb8587b70577ba07f58802ae0
Sean Anggani <seanvang@umich.edu>
2020-01-21 15:54:09 +0700

    oath success

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./config/keys')
 const app = express()
-const port = 3000
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('acc ', accessToken)
+    console.log('ref ', refreshToken)
+    console.log('prof ', profile)
+}));
+//need client id and client secret for google strategy
 
-app.get('/', (req, res) => res.send('Hello heck World!'))
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
